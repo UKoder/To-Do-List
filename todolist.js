76dec0c5ab75661e2a79dd0cb3b7f5b6371643ae
UK <ukstudies27@gmail.com>
2025-07-18 19:20:42 +0530

    Version-1

---
+++ todolist.js
@@ -0,0 +1,38 @@
+let todo = JSON.parse(localStorage.getItem("todo"));
+if (!Array.isArray(todo)) {
+  todo = [];
+}
+
+renderTodo();
+
+function addTodo() {
+  let todoName = document.querySelector(".js-todo-input").value;
+  let dueDate = document.querySelector(".js-dueDate").value;
+
+  if (todoName.trim() === "") return;
+  todo.push({
+    name: todoName,
+    dueDate,
+  });
+  localStorage.setItem("todo", JSON.stringify(todo));
+
+  document.querySelector(".js-todo-input").value = "";
+  document.querySelector(".js-dueDate").value = "";
+
+  renderTodo();
+}
+
+function renderTodo() {
+  let html = "";
+  for (let i = 0; i < todo.length; i++) {
+    html += `
+    <div>${todo[i].name}</div>
+    <div>${todo[i].dueDate}</div>
+    <button onclick = "
+    todo.splice(${i},1);
+    localStorage.setItem('todo', JSON.stringify(todo));
+    renderTodo();"
+    class='js-delBtn'>Delete</button>`;
+  }
+  document.querySelector(".js-todo").innerHTML = html;
+}
